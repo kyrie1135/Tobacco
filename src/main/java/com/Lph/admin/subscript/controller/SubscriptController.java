99069b9bf0935414a0fd619ede04ea9bbea03acd
liupenghao <1135030547@qq.com>
2020-02-20 19:33:28 +0800

    2019-2-20

--- src/main/java/com/Lph/admin/subscript/controller/SubscriptController.java
+++ src/main/java/com/Lph/admin/subscript/controller/SubscriptController.java
@@ -1,7 +1,6 @@
 package com.Lph.admin.subscript.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
