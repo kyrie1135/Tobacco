c13b4d41652448bd51d9ba2fcbabe7909d145a4f
Kyrie1135 <1135030547@qq.com>
2020-02-23 15:35:58 +0800

    2020-2-23 随机id生成器

---
+++ src/main/java/com/Lph/admin/Utils/IdUtil.java
@@ -0,0 +1,20 @@
+package com.Lph.admin.Utils;
+
+import java.util.Random;
+
+/**
+ * 生成不重复随机id
+ */
+public class IdUtil {
+    public static String nextId() {
+        // 取当前时间的长整形值包含毫秒
+        long millis = System.currentTimeMillis();
+        // long millis = System.nanoTime();
+        // 加上三位随机数
+        Random random = new Random();
+        int end3 = random.nextInt(999);
+        // 如果不足三位前面补0
+        String str = millis + String.format("%03d", end3);
+        return str;
+    }
+}
